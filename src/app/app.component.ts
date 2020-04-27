9730e58fc949457a1ef4554d7b4028322024ebc7
Gourav <aroragourav1992@gmail.com>
2020-04-28 01:01:08 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageServiceService } from './storage-service.service';
+import { Note } from './models/Note';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'NotesMap';
+export class AppComponent implements OnInit{
+
+  allNotes: Note[];
+  noteText;
+
+  constructor(private readonly storageService: StorageServiceService){
+
+  }
+  
+  private submit = () => {
+    if( this.noteText ){
+      this.storageService.addNote({
+        text: this.noteText
+      });
+      this.noteText = '';
+    }
+  }
+
+  ngOnInit(){
+    this.storageService.getAllNotes()
+    .subscribe((data: Note[]) => {
+      console.log('Here');
+      this.allNotes = data;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListNotesComponent } from './list-notes/list-notes.component';
+import { NotesComponent } from './notes/notes.component';
+
+import { FormsModule } from '@angular/forms'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListNotesComponent,
+    NotesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
